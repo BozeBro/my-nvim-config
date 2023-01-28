3da425f43526c446c4ac6b60c08b6b884d38c426
BozeBro <ben.ozua@gmail.com>
2023-01-27 19:47:34 -0500

    Added lazygit terminal

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,14 +1,18 @@
 return {
-    {
-        "akinsho/toggleterm.nvim",
-        version = "*",
-        config = true,
-        lazy = true,
-        cmd = "ToggleTerm",
-        keys = {
-            { "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle Float Terminal" } },
-            { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle Horizontal Terminal" } },
-            { "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "Toggle Vertical Terminal" } },
-        },
-    },
+	{
+		"akinsho/toggleterm.nvim",
+		version = "*",
+		config = function()
+			local toggle = require("toggleterm")
+			toggle.setup()
+		end,
+		lazy = true,
+		cmd = { "ToggleTerm", "TermExec" },
+		keys = {
+			{ "<leader>tf", "<cmd>ToggleTerm direction=float<cr>", { desc = "Toggle Float Terminal" } },
+			{ "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", { desc = "Toggle Horizontal Terminal" } },
+			{ "<leader>tv", "<cmd>ToggleTerm size=80 direction=vertical<cr>", { desc = "Toggle Vertical Terminal" } },
+			{ "<leader>tl", "<cmd>TermExec cmd='lazygit' dir=./ direction=float <cr>", { desc = "Toggle Lazygit" } },
+		},
+	},
 }
