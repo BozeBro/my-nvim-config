77e1f068d8d4fb98f9277828c9a46cd529f97b7f
BozeBro <ben.ozua@gmail.com>
2023-02-11 16:49:03 -0500

    Added new colortheme catpuccin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,32 @@
 return {
 	{
 		"folke/tokyonight.nvim",
-		lazy = false,
-		priority = 1000,
+		-- lazy = false,
+		-- priority = 1000,
 		opts = {
 			style = "night",
 			transparent = true,
 		},
 		config = function(_, opts)
-			require("tokyonight").load(opts)
+			-- require("tokyonight").load(opts)
 			-- require("gruvbox").load()
 		end,
 	},
 	{ "ellisonleao/gruvbox.nvim", lazy = true },
+	{
+		"https://github.com/catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false,
+		opts = {
+			flavour = "macchiato",
+			transparent_background = true,
+		},
+		priority = 1000,
+		config = function(_, opts)
+			require("catppuccin").setup(opts)
+			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
 	{ "nvim-tree/nvim-web-devicons", lazy = true, config = true },
 	{ "tpope/vim-surround", event = "BufEnter", lazy = false },
 	{ "tpope/vim-repeat", event = "BufEnter", lazy = true },
