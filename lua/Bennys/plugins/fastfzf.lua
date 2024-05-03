897c5a48f5993fa48d85c7b947b04137cdb6515a
BozeBro <ben.ozua@gmail.com>
2024-05-03 05:06:30 -0400

    reinstalling fzf

--- lua/Bennys/plugins/fastfzf.lua
+++ lua/Bennys/plugins/fastfzf.lua
@@ -1,9 +1,23 @@
+local map = vim.keymap.set
 return {
 	{
 		"ibhagwan/fzf-lua",
-		enabled = false,
-		lazy = true,
+		enabled = true,
+		-- lazy = true,
 		-- optional for icon support
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("fzf-lua")
+			map("n", "<leader>ff", "<cmd>FzfLua files<cr>", { desc = "Fast FZF" })
+			map("n", "<leader>fg", "<cmd>FzfLua grep<cr>", { desc = "Fast FZF grep" })
+			map("n", "<leader>fb", "<cmd>FzfLua buffers<cr>", { desc = "Fast FZF buffers" })
+			map("n", "<leader>/", "<cmd>FzfLua lgrep_curbuf<cr>", { desc = "Fast FZF live grep current buffer" })
+			map(
+				"n",
+				"<leader>ft",
+				"<cmd>FzfLua lsp_document_symbols<cr>",
+				{ desc = "Fast FZF lsp document symbols (treesitter like)" }
+			)
+		end,
 	},
 }
